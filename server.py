aa48ebe56c412c5f20a9bf4432f5c7b99fb21f17
Daniyal45 <daniyal_09.2005@hotmail.com>
2022-03-05 14:15:39 +0500

    new csv generator

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 from flask import Flask, request
-from flask_cors import CORS, cross_origin
 from csv_generator import generate_csv
 import json
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/predict')
 def hello_world():
